1035f33e2e9aff6105c5b75c6939009f275a5222
Pedro-Benevides <pvsbenevides197@gmail.com>
2023-04-27 13:20:32 -0300

    feat(TCPClient): implementa atributos de socket, getters, método de conexao e main com solicitações básicas ao servidor

--- src/Classes/TCPClient.java
+++ src/Classes/TCPClient.java
@@ -1,11 +1,77 @@
-public class TCPClient implements Client {
+import java.io.*;
+import java.net.*;
+
+public class TCPClient implements Client, Jogador {
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Socket socket;
     public JogadaEnum jogada;
 
-    public void ConnectServer(){
+    public void ConnectServer() {
+        // Criacao do socket do Cliente, acessando o endereco/porta do Servidor e
+        // criacao dos fluxos de I/O. (inicio)
+        try {
+            int serverPort = 8000;
+            socket = new Socket("127.0.0.1", serverPort);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            // Criacao do socket do Cliente, acessando o endereco/porta do Servidor e
+            // criacao dos fluxos de I/O. (final)
 
+            // Tratamento de Excecoes. (inicio)
+        } catch (UnknownHostException e) {
+            System.out.println("Sock:" + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        }
+        // Tratamento de Excecoes. (final)
     }
 
-    public void Lance(JogadaEnum lance) {
-        jogada = lance;
+    public DataInputStream getIn() {
+        return in;
+    }
+
+    public DataOutputStream getOut() {
+        return out;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void Lance(JogadaEnum jogada) {
+        this.jogada = jogada;
+    }
+
+    public static void main(String[] args) {
+        TCPClient client = new TCPClient();
+
+        try {
+            // Conversa entre o Cliente e o Servidor. (inicio)
+            int i = 0;
+            for (int j = 0; j < 10; ++j) {
+                client.getOut().writeUTF("Pedido de Servico " + ++i);
+                String data = client.getIn().readUTF();
+                System.out.println("Recebido a " + data);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread Interrupted:" + e.getMessage());
+                }
+            }
+            // Conversa entre o Cliente e o Servidor. (final)
+
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            if (client.getSocket() != null)
+                try {
+                    client.getSocket().close();
+                } catch (IOException e) {
+                    System.out.println("IO:" + e.getMessage());
+                }
+        }
     }
 }
