701520d03f5aece8ec583726bcbf5f068e38d71c
Pedro-Benevides <pvsbenevides197@gmail.com>
2023-05-07 18:32:57 -0300

    refactor(MoveEnum): reestrutura enum de jogadas

--- src/Enums/MoveEnum.java
+++ src/Enums/MoveEnum.java
@@ -1,13 +1,22 @@
-public enum MoveEnum {
-    PEDRA(1), PAPEL(2), TESOURA(3);
+enum MoveEnum {
+    PEDRA,
+    PAPEL,
+    TESOURA;
 
-    private final int move;
+    public static MoveEnum getMove(int moveValue) {
+        MoveEnum move = null;
+        switch (moveValue) {
+            case 1:
+                move = MoveEnum.PEDRA;
+                break;
+            case 2:
+                move = MoveEnum.PAPEL;
+                break;
+            case 3:
+                move = MoveEnum.TESOURA;
+                break;
+        }
 
-    MoveEnum(int moveValue) {
-        move = moveValue;
-    }
-
-    public int getMove() {
         return move;
     }
 }
